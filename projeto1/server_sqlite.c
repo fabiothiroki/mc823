cb8a4daf3ed77ca35cfbf46ba09dc4332ed9fa51
fabiothiroki <fabiothiroki@gmail.com>
2013-04-12 09:48:41 -0300

    funcao loadbooks

--- projeto1/server_sqlite.c
+++ projeto1/server_sqlite.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 #include <sqlite3.h>
+#include <string.h>
 
-int main(void) {
+typedef struct bookStruct {
+  char isbn[11];		
+  char title[50];
+  char author[50];
+  char description[300];
+  char publisher[50];
+  char year[3];	
+  char horarios[18];	
+  char quantity[2];	
+} Book;
+
+Book arr_books[10];
+
+void loadBooks(){
 
 	sqlite3 *conn;
 	sqlite3_stmt *res;
@@ -16,11 +30,25 @@ int main(void) {
 		printf("Can not open database");
 	}
 
-	 error = sqlite3_prepare_v2(conn, "select title from books order by title",1000, &res, &tail);
+	 error = sqlite3_prepare_v2(conn, "select title,author,description from books order by title",1000, &res, &tail);
 
+	 int i = 0;
 	 while (sqlite3_step(res) == SQLITE_ROW) {
-	 	printf("%s \n",sqlite3_column_text(res, 0));
+
+	 	strcpy(arr_books[i].title,sqlite3_column_text(res, 0));
+	 	strcpy(arr_books[i].author,sqlite3_column_text(res, 1));
+	 	strcpy(arr_books[i].description,sqlite3_column_text(res, 2));
+
+	 	printf("\n");
+	 	printf("%s \n",arr_books[i].title);
+	 	printf("%s \n",arr_books[i].author);
 	 }
+
+}
+
+int main(void) {
+
+	loadBooks();
 
 	return 0;
 }
